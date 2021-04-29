278f3f41f93d648b43cfcf6345af428ea8c02d54
prog_zidane <mohamed.zidane95@gmail.com>
2021-04-29 04:49:07 +0200

    Create Project

---
+++ SwiftyNetTests/SwiftyNetTests.swift
@@ -0,0 +1,33 @@
+//
+//  SwiftyNetTests.swift
+//  SwiftyNetTests
+//
+//  Created by prog_zidane on 4/29/21.
+//
+
+import XCTest
+@testable import SwiftyNet
+
+class SwiftyNetTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
